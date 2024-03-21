4a3b9b3949f7d2a7c89f7a33eeb0edd275c66c82
Sakin790 <sakinislam79@gmail.com>
2024-03-21 12:48:00 +0600

    till dot env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: ".env",
+  });
+
 
 const app = express();
 app.use(
@@ -13,3 +20,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server in running at PORT : ${process.env.PORT}`);
+  });
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,4 +1,9 @@
 import multer from "multer";
+import path, { extname } from "path";
+import { apiError } from "../utils/apiError.js";
+
+const MAX_FILE_SIXE = 2097150;
+const ALLOWED_FILE_FORMAT = ["jpg", "png", "jpeg"];
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -9,4 +14,17 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-export const upload = multer({ storage: storage });
+
+const filterFile = (req, res, cb) => {
+  const extname = path.extname(file.originalname);
+  if (!ALLOWED_FILE_FORMAT.includes(extname.substring(1))) {
+    return new apiError(401, "Invalid photo format..!");
+  }
+  cb(null, true);
+};
+
+export const upload = multer({
+  storage: storage,
+  limits: MAX_FILE_SIXE,
+  filterFile,
+});
